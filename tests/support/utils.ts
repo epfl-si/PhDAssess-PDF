8b6f79f291165d8378c8e65e07e5eb87b68e8eb1
Julien Delasoie <julien.delasoie@epfl.ch>
2021-08-04 16:20:06 +0200

    Remove unused files

--- tests/support/utils.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Set the first letter of the keys to be lowercase
- */
-export function dictKeysToLowerCase(obj: any) {
-  return Object.fromEntries(
-    Object.entries(obj).map(([k, v]) => [k.charAt(0).toLowerCase() + k.slice(1), v])
-  )
-}
