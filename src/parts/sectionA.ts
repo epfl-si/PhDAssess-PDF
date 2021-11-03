d38c42f755ca8262af485ffda9868a6a90c7fb7b
lvenries <luc@lutine.ch>
2021-11-03 13:31:55 +0100

    Delete courses table and check if items is empty

--- src/parts/sectionA.ts
+++ src/parts/sectionA.ts
@@ -10,7 +10,8 @@ export default function getSectionA(phdVariables: IInputVariables) {
     const nameOfCandidate: Content = { text:[{text: 'Name of candidate: ',  bold: true}, phdVariables.phdStudentName ]}
     const tentativeThesisTitle: Content = { text: [{text: 'Tentative thesis title: ', bold: true}, {text: phdVariables.tentativeThesisTitle }]}
     const phDThesisDirector: Content = { text: [{text: 'Thesis director: ', bold: true}, {text: phdVariables.thesisDirectorName }]}
-    const phDThesisCoDirector: Content = { text: [{text: 'Thesis co-director: ', bold: true}, {text: phdVariables.thesisCoDirectorName }]}
+    const phDThesisCoDirector: Content = phdVariables.thesisCoDirectorName ?
+        { text: [{text: 'Thesis co-director: ', bold: true}, {text: phdVariables.thesisCoDirectorName }]} : "";
     const dateOfCandidacyExam: Content = { text: [{text: 'Date of candidacy exam: ', bold: true}, {text: phdVariables.dateOfCandidacyExam }, seperator]}
 
     const taDutiesHoursAndOrCoursesCompleted : Content = { text: [{text: 'TA duties ',bold: true}, {text: '(Hours and/or courses completed): '}, {text: phdVariables.taDutiesHoursAndOrCoursesCompleted }, seperator]}
@@ -19,36 +20,7 @@ export default function getSectionA(phdVariables: IInputVariables) {
                                             {text: 'Credits needed:  '}, {text: phdVariables.creditsNeeded },
                                             {text: '          Credits completed:  '}, {text: phdVariables.totalCreditsObtained },
                                             {text: '          Credits planned:  '}, {text: phdVariables.totalCreditsPlanned}, seperator]}
-    const listBelowObtainedCredits = {text : 'Please list below obtained credits:'}
-    const listcoursesCreditsCompleted: Content= {
-        style: 'table',
-        table: {
-            headerRows: 1,
-            body: [
-              [{text: 'Year / semester', style: 'tableHeader'}, {text: 'Course (name and number)', style: 'tableHeader'}, {text: 'Credits', style: 'tableHeader'}],
-              ...Array.from(phdVariables.listBelowObtainedCredits, (course : IInputVariables) => [
-                {text: `${course.yearObtained} / ${course.semestreObtained}`},
-                {text: course.courseNumberNameObtained},
-                {text: course.creditsObtained},
-              ]),
-            ]
-          }
-        }
-        const listBelowPlannedCourses = {text : 'Please list below planned courses:'}
-        const listcoursesCreditsPlanned: Content= {
-            style: 'table',
-            table: {
-                headerRows: 1,
-                body: [
-                  [{text: 'Year / semester', style: 'tableHeader'}, {text: 'Course (name and number)', style: 'tableHeader'}, {text: 'Credits', style: 'tableHeader'}],
-                  ...Array.from(phdVariables.listBelowPlannedCourses, (course : IInputVariables) => [
-                    {text: `${course.yearPlanned} / ${course.semestrePlanned}`},
-                    {text: course.courseNumberNamePlanned},
-                    {text: course.creditsPlanned},
-                  ]),
-                ]
-              }
-            }
+
 
     return [
         seperator,
@@ -61,10 +33,7 @@ export default function getSectionA(phdVariables: IInputVariables) {
         dateOfCandidacyExam,
         taDutiesHoursAndOrCoursesCompleted,
         coursesCredits,
-        listBelowObtainedCredits,
-        listcoursesCreditsCompleted,
-        listBelowPlannedCourses,
-        listcoursesCreditsPlanned,
+
 
     ]
 }
--- src/parts/sectionD.ts
+++ src/parts/sectionD.ts
@@ -6,18 +6,21 @@ import {seperator} from "./utils"
 export default function getSectionA(phdVariables: IInputVariables) {
 
     const section: Content = [{text: 'Overall appraisal and signatures   ', style: 'header'}]
-    const intro: Content = { text: [{ text:'By signing the Annual Report the doctoral student and the thesis director(s) agree that the discussion regarding the achieved and planned objectives as well as the progress assessment satisfy and are approved by both parties.'}, seperator]}
-    const intro2: Content = { text: [{text: 'For the doctoral student: ',  bold: true},{ text:'Your thesis director(s) is responsible for guiding you in your research and for coaching you to reach the scientific level of a PhD. The critical feedback of your thesis director(s) plays a key role in this process and thus also for your success beyond. Approval of the Annual Report indicates that you have discussed with your thesis director(s) and understand the expectations for improvement in the areas identified, and that you agree on the plan to reach these milestones and to assess your progress.'}, seperator]}
-    const intro3: Content = { text: [{text: 'For the thesis director: ',  bold: true},{ text:'Your signature indicates that you have discussed all the above points with your doctoral student and considered their feedback regarding any potential problem, you have explained your expectations for improvement, when appropriate, including mechanisms for intermediate progress assessments, and you have discussed a plan for timely publication of the results and for thesis defence.'}, seperator]}
+    const intro: Content = { text: [{ text:'By validating the Annual Report (checkbox), the doctoral student and the thesis director(s) agree that the discussion regarding the achieved and planned objectives as well as the progress assessment satisfy and are approved by both parties.'}, seperator]}
+    const intro2: Content = { text: [{text: 'For the doctoral student: ',  bold: true},{ text:'Your thesis director(s) is responsible for guiding you in your research and for coaching you to reach the scientific level of a PhD. The critical feedback of your thesis director(s) plays a key role in this process and thus also for your success beyond. Validation of the Annual Report indicates that you have discussed with your thesis director(s) and understand the expectations for improvement in the areas identified, and that you agree on the plan to reach these milestones and to assess your progress.'}, seperator]}
+    const intro3: Content = { text: [{text: 'For the thesis director: ',  bold: true},{ text:'Your validation indicates that you have discussed all the above points with your doctoral student and considered their feedback regarding any potential problem, you have explained your expectations for improvement, when appropriate, including mechanisms for intermediate progress assessments, and you have discussed a plan for timely publication of the results and for thesis defence.'}, seperator]}
     const generalAppraisalOfTheProgress: Content = { text:[{text: 'General appraisal of the progress: ',  bold: true}, {text: phdVariables.generalAppraisalOfTheProgress} ,seperator ]}
-    const agreeThesisCoDirector: Content = { text:[{text: 'Thesis Co-Director review: ',  bold: true}, {text : '\n'}, {text: phdVariables.thesisCoDirectorDateOfReview}, {text : '       '}, {text: phdVariables.agreeThesisCoDirector} ]}
-    const thesisCoDirectorComment: Content = { text: [{text : 'Comment: '}, {text: phdVariables.thesisCoDirectorComment},seperator ]}
+    const agreeThesisCoDirector: Content = phdVariables.thesisCoDirectorName ?
+        { text:[{text: 'Thesis Co-Director review: ',  bold: true}, {text : '\n'}, {text: phdVariables.thesisCoDirectorDateOfReview}, {text : '       '}, {text: phdVariables.agreeThesisCoDirector} ]} : ""
+    const thesisCoDirectorComment: Content = phdVariables.thesisCoDirectorName ?
+        { text: [{text : 'Comment: '}, {text: phdVariables.thesisCoDirectorComment},seperator ]} : ""
     const agreeThesisDirector: Content = { text:[{text: 'Thesis Director review: ',  bold: true}, {text : '\n'}, {text: phdVariables.thesisDirectorDateOfReview}, {text : '       '}, {text: phdVariables.agreeThesisDirector} ]}
     const thesisDirectorComment: Content = { text: [{text : 'Comment: '}, {text: phdVariables.thesisDirectorComment},seperator ]}
     const agreePhd: Content = { text:[{text: 'Doctoral Student review: ',  bold: true}, {text : '\n'}, {text: phdVariables.phdDateOfReview}, {text : '       '}, {text: phdVariables.agreePhd} ]}
     const phdComment: Content = { text: [{text : 'Comment: '}, {text: phdVariables.phdComment},seperator ]}
     const programDirectorReview: Content = { text:[{text: 'Program Director ',  bold: true}, {text : 'checked the report on '}, {text: phdVariables.programDirectorDate} ]}
-    const mentorMeet: Content = { text:[{text: 'Mentor and doctoral student have met ',  bold: true}, {text : '\nValidate by the mentor on '}, {text: phdVariables.mantorDate}, {text : '\nValidate by the doctoral student on '}, {text: phdVariables.phdDate} ]}
+    const programDirectorphdComment: Content = { text: [{text : 'Comment: '}, {text: phdVariables.programDirectorComment}, seperator ]}
+    const mentorMeet: Content = { text:[{text: 'Mentor and doctoral student have met ',  bold: true}, {text : '\nValidate by the mentor on '}, {text: phdVariables.mentorDate}, {text : '\nValidate by the doctoral student on '}, {text: phdVariables.phdDate} ]}
 
     return [
         section,
@@ -32,6 +35,7 @@ export default function getSectionA(phdVariables: IInputVariables) {
         agreePhd,
         phdComment,
         programDirectorReview,
+        programDirectorphdComment,
         mentorMeet,
     ]
 }
