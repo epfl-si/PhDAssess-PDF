dfae30f152bfbe9be5d3a8018cf6fc212f70f85e
lvenries <luc@lutine.ch>
2021-11-15 14:48:53 +0100

    display Program Direcor name

--- src/parts/sectionA.ts
+++ src/parts/sectionA.ts
@@ -9,9 +9,10 @@ export default function getSectionA(phdVariables: IInputVariables) {
     const section: Content = [{text: 'Basic information', style: 'header'}]
     const nameOfCandidate: Content = { text:[{text: 'Name of candidate: ',  bold: true}, phdVariables.phdStudentName, {text: ' ('}, {text: phdVariables.phdStudentSciper } , {text: ')'} ]}
     const tentativeThesisTitle: Content = { text: [{text: 'Tentative thesis title: ', bold: true}, {text: phdVariables.tentativeThesisTitle }]}
-    const phDThesisDirector: Content = { text: [{text: 'Thesis director: ', bold: true}, {text: phdVariables.thesisDirectorName }]}
-    const phDThesisCoDirector: Content = phdVariables.thesisCoDirectorName ?
+    const phdThesisDirector: Content = { text: [{text: 'Thesis director: ', bold: true}, {text: phdVariables.thesisDirectorName }]}
+    const phdThesisCoDirector: Content = phdVariables.thesisCoDirectorName ?
         { text: [{text: 'Thesis co-director: ', bold: true}, {text: phdVariables.thesisCoDirectorName }]} : "";
+    const phdProgramDirector: Content = { text: [{text: 'Program director: ', bold: true}, {text: phdVariables.programDirectorName }]}
     const dateOfCandidacyExam: Content = { text: [{text: 'Date of candidacy exam: ', bold: true}, {text: phdVariables.dateOfCandidacyExam }, seperator]}
 
     const taDutiesHoursAndOrCoursesCompleted : Content = { text: [{text: 'TA duties ',bold: true}, {text: '(activities done): '}, {text: phdVariables.taDutiesHoursAndOrCoursesCompleted }, seperator]}
@@ -28,8 +29,9 @@ export default function getSectionA(phdVariables: IInputVariables) {
         section,
         nameOfCandidate,
         tentativeThesisTitle,
-        phDThesisDirector,
-        phDThesisCoDirector,
+        phdThesisDirector,
+        phdThesisCoDirector,
+        phdProgramDirector,
         dateOfCandidacyExam,
         taDutiesHoursAndOrCoursesCompleted,
         coursesCredits,
--- src/parts/sectionD.ts
+++ src/parts/sectionD.ts
@@ -39,6 +39,9 @@ export default function getSectionA(phdVariables: IInputVariables) {
     const programDirectorReview: Content = { text:[{text: 'Program director ',  bold: true}, {text : 'checked the report on '}, {text: phdVariables.programDirectorDate} ]}
     const programDirectorphdComment: Content = { text: [{text : 'Comment: '}, {text: phdVariables.programDirectorComment}, seperator ]}
     const mentorMeet: Content = { text:[{text: 'Mentor and doctoral student have met ',  bold: true}, {text : '\nValidated by the mentor on '}, {text: phdVariables.mentorDate}, {text : '\nValidated by the doctoral student on '}, {text: phdVariables.phdDate}, seperator  ]}
+    const today = new Date()
+    const currentDay = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`
+    const datePdf : Content = { text: [seperator, {text : 'EPFL, the '}, {text: currentDay},seperator ]}
 
     return [
         section,
@@ -55,5 +58,6 @@ export default function getSectionA(phdVariables: IInputVariables) {
         mentorMeet,
         programDirectorReview,
         programDirectorphdComment,
+        datePdf,
     ]
 }
