48d11de4d5c30667a03292a477de8db6f26b4be2
lvenries <luc@lutine.ch>
2022-02-18 08:26:31 +0100

    add separator

--- src/parts/provisional.ts
+++ src/parts/provisional.ts
@@ -1,10 +1,11 @@
 import {Content} from "pdfmake/interfaces"
+import {seperator} from "./utils"
 
 
 
 export default function getProvisional() {
 
-    const ongoing: Content = [{text: 'Please note that it is not the final version of the annual report because the process is still ongoing.',  bold: true} ]
+    const ongoing: Content = [{text: 'Please note that it is not the final version of the annual report because the process is still ongoing.',  bold: true},seperator ]
 
     return [
       ongoing,
