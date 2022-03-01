f86c9b3823903a7522015c0e0e4cb6e6cc6e16b3
lvenries <luc@lutine.ch>
2022-03-01 16:59:30 +0100

    PHDAR-89 space coorection

--- src/parts/provisional.ts
+++ src/parts/provisional.ts
@@ -4,7 +4,7 @@ import {seperator} from "./utils"
 
 export default function getProvisional() {
 
-    const ongoing: Content = [{text: 'Please note that it is not the final version of the annual report because the process is still ongoing.',  bold: true},seperator ]
+    const ongoing: Content = {text: [{text: 'Please note that it is not the final version of the annual report because the process is still ongoing.',  bold: true}, seperator ]}
 
     return [
       ongoing,
