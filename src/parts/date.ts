d6d2e80b2ca289c90ba8f646eacdec6998ca97e7
lvenries <luc@lutine.ch>
2022-03-01 17:13:07 +0100

    change date format

--- src/parts/date.ts
+++ src/parts/date.ts
@@ -5,7 +5,7 @@ import {seperator} from "./utils"
 
 export default function getDate() {
     const today = new Date()
-    const currentDay = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`
+    const currentDay = `${today.getDate()}.${today.getMonth()+1}.${today.getFullYear()}`
     const datePdf : Content = { text: [seperator, {text : 'EPFL, the '}, {text: currentDay},seperator ]}
 
     return [
--- src/parts/footer.ts
+++ src/parts/footer.ts
@@ -8,8 +8,6 @@ export default function getFooter(phdVariables: IInputVariables) {
     const programDirectorReview: Content = { text:[{text: 'Program director ',  bold: true}, {text : 'checked the report on '}, {text: phdVariables.programDirectorDate} ]}
     const programDirectorphdComment: Content = { text: [{text : 'Comment: '}, {text: phdVariables.programDirectorComment}, seperator ]}
     const mentorMeet: Content = { text:[{text: 'Mentor and doctoral student have met ',  bold: true}, {text : '\nValidated by the mentor on '}, {text: phdVariables.mentorDate}, {text : '\nValidated by the doctoral student on '}, {text: phdVariables.phdDate}, seperator  ]}
-    const today = new Date()
-    const currentDay = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`
 
     return [
         mentorMeet,
