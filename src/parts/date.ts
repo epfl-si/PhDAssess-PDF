c069bae520d6253bb4360f62af8bb7e10736b01f
Julien Delasoie <julien.delasoie@epfl.ch>
2022-03-02 14:03:16 +0100

    Fix date on signature

--- src/parts/date.ts
+++ src/parts/date.ts
@@ -4,7 +4,7 @@ import {seperator} from "./utils"
 
 export default function getDate() {
     const today = new Date()
-    const currentDay = `${('0' + today.getDate()).slice(-2)}.${('0' + today.getMonth()+1).slice(-2)}.${today.getFullYear()}`
+    const currentDay = `${('0' + today.getDate()).slice(-2)}.${('0' + (today.getMonth() +1)).slice(-2)}.${today.getFullYear()}`
     const datePdf : Content = { text: [seperator, {text : 'EPFL, the '}, {text: currentDay},seperator ]}
 
     return [
