df0f2450238605c91b4a26c44a1210ab3a3ce2b3
Julien Delasoie <julien.delasoie@epfl.ch>
2022-03-02 13:37:11 +0100

    Add 0 on the date for the signature

--- src/parts/date.ts
+++ src/parts/date.ts
@@ -1,11 +1,10 @@
 import {Content} from "pdfmake/interfaces"
-import {IInputVariables} from "zeebe-node"
 import {seperator} from "./utils"
 
 
 export default function getDate() {
     const today = new Date()
-    const currentDay = `${today.getDate()}.${today.getMonth()+1}.${today.getFullYear()}`
+    const currentDay = `${('0' + today.getDate()).slice(-2)}.${('0' + today.getMonth()+1).slice(-2)}.${today.getFullYear()}`
     const datePdf : Content = { text: [seperator, {text : 'EPFL, the '}, {text: currentDay},seperator ]}
 
     return [
