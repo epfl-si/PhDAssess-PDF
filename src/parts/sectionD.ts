11655678d83e8a27aa0b7247eae9d4188fe8496b
Julien Delasoie <julien.delasoie@epfl.ch>
2023-09-14 14:30:56 +0200

    fix: Remove unsued intro on section D

--- src/parts/sectionD.ts
+++ src/parts/sectionD.ts
@@ -6,14 +6,11 @@ import {seperator} from "./utils"
 export default function getSectionD(phdVariables: IInputVariables) {
 
     const section: Content = [{text: 'Overall appraisal and signatures   ', style: 'header'}]
-    const intro: Content = { text: [{ text:'By validating the Annual Report (checkbox), the doctoral candidate and the thesis director(s) agree that the discussion regarding the achieved and planned objectives as well as the progress assessment satisfy and are approved by both parties.'}, seperator]}
     const generalAppraisalOfTheProgress: Content = { text:[{text: 'General appraisal of the progress: ',  bold: true}, {text: phdVariables.generalAppraisalOfTheProgress} ,seperator ]}
 
 
     return [
         section,
-        intro2,
-        intro3,
         generalAppraisalOfTheProgress,
     ]
 }
