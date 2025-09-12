4f304006c148076634b6fa8b88000a9191f2e27d
Julien Delasoie <julien.delasoie@epfl.ch>
2025-09-12 11:24:59 +0200

    refactor: Simplify variable assignation logic

--- src/parts/sectionDAgree.ts
+++ src/parts/sectionDAgree.ts
@@ -5,39 +5,33 @@ import {seperator} from "./utils"
 
 export default function getSectionDAgree(phdVariables: IInputVariables) {
 
-     
-    if (phdVariables.agreeThesisCoDirector == "Thesis co-director agrees") {
-        var agreeThesisCoDirectorOn = " agreed on the report on "
-    }
-    else {
-        var agreeThesisCoDirectorOn = " did not agree on the report on "
-    }
+    const agreeThesisCoDirectorOn = (phdVariables.agreeThesisCoDirector == "Thesis co-director agrees") ?
+      " agreed on the report on " :
+      " did not agree on the report on "
 
     const agreeThesisCoDirector: Content = phdVariables.thesisCoDirectorDateOfReview ?
         { text:[{text: 'Thesis co-director',  bold: true}, {text : agreeThesisCoDirectorOn}, {text: phdVariables.thesisCoDirectorDateOfReview} ]} : ""
     const thesisCoDirectorComment: Content = phdVariables.thesisCoDirectorComment ?
         { text: [{text : 'Comment: '}, {text: phdVariables.thesisCoDirectorComment},seperator ]} : ""
-    if (phdVariables.agreeThesisDirector == "Thesis director agrees") {
-          var agreeThesisDirectorOn = " agreed on the report on "
-        }
-    else {
-          var agreeThesisDirectorOn = " did not agree on the report on "
-        }
-    
+
+    const agreeThesisDirectorOn = (phdVariables.agreeThesisDirector == "Thesis director agrees") ?
+      " agreed on the report on " :
+      " did not agree on the report on "
+
     const agreeThesisDirector: Content = phdVariables.thesisDirectorDateOfReview ?
         { text:[{text: 'Thesis director ',  bold: true}, {text : agreeThesisDirectorOn}, {text: phdVariables.thesisDirectorDateOfReview} ]} : ""
     const thesisDirectorComment: Content = phdVariables.thesisDirectorComment ?
-        { text: [{text : 'Comment: '}, {text: phdVariables.thesisDirectorComment},seperator ]} : ""                                                         
-    if (phdVariables.agreePhd == "Doctoral candidate agrees" || phdVariables.agreePhd == "Doctoral student agrees") {
-        var agreeaPhdOn = " agreed on the report on "
-    }
-    else {
-        var agreeaPhdOn = " did not agree on the report on "
-    }
+        { text: [{text : 'Comment: '}, {text: phdVariables.thesisDirectorComment},seperator ]} : ""
+
+    const agreeaPhdOn = (
+      phdVariables.agreePhd == "Doctoral candidate agrees" || phdVariables.agreePhd == "Doctoral student agrees"
+    ) ?
+      " agreed on the report on " :
+      " did not agree on the report on "
+
     const agreePhd: Content = { text:[{text: 'Doctoral candidate',  bold: true}, {text : agreeaPhdOn}, {text: phdVariables.phdDateOfReview} ]}
     const phdComment: Content =  phdVariables.phdComment ?
-        { text: [{text : 'Comment: '}, {text: phdVariables.phdComment},seperator ]} : {text : '\n\n'} 
-
+        { text: [{text : 'Comment: '}, {text: phdVariables.phdComment},seperator ]} : {text : '\n\n'}
 
     return [
         agreeThesisCoDirector,
@@ -47,6 +41,5 @@ export default function getSectionDAgree(phdVariables: IInputVariables) {
         agreePhd,
         phdComment,
         seperator,
-
     ]
 }
